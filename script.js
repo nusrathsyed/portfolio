dd376c7959854a06770b41a8b7002f4aa0df6746
Nusrath Syed <84590545+nusrathsyed@users.noreply.github.com>
2025-01-17 21:09:49 -0500

    changed layout

--- script.js
+++ script.js
@@ -1,49 +1,34 @@
-//Menu functions for the Home Projects and Skills section
+// Menu functions for the Home Projects and Skills section
 function toggleMenu() {
     const navLinks = document.getElementById('nav-links');
-    navLinks.classList.toggle('hidden');  // This will toggle the visibility of the navigation links
-}
-
-// Track mouse position relative to the entire document
-document.addEventListener('mousemove', (e) => {
+    navLinks.classList.toggle('hidden'); // This will toggle the visibility of the navigation links
+  }
+  
+  // Track mouse position relative to the entire document
+  document.addEventListener('mousemove', (e) => {
     const mouseX = e.clientX;
     const mouseY = e.clientY;
-
+  
     // Update CSS variables with the mouse position
     document.documentElement.style.setProperty('--mouse-x', `${mouseX}px`);
     document.documentElement.style.setProperty('--mouse-y', `${mouseY}px`);
-});
-
-// Ensure the effect stays at the right position when the page scrolls
-document.addEventListener('scroll', () => {
-    const mouseX = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--mouse-x'));
-    const mouseY = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--mouse-y'));
-
-    document.documentElement.style.setProperty('--mouse-x', `${mouseX}px`);
-    document.documentElement.style.setProperty('--mouse-y', `${mouseY}px`);
-});
-
-// Function to handle the 'View Code' link click event
-const codeLinks = document.querySelectorAll('a');
-
-codeLinks.forEach(link => {
+  });
+  
+  // Function to handle the 'View Code' link click event
+  const codeLinks = document.querySelectorAll('a');
+  codeLinks.forEach(link => {
     link.addEventListener('click', function() {
-        // Remove the 'clicked' class from all links
-        codeLinks.forEach(l => l.classList.remove('clicked'));
-
-        // Add the 'clicked' class to the clicked link
-        this.classList.add('clicked');
+      // Remove the 'clicked' class from all links
+      codeLinks.forEach(l => l.classList.remove('clicked'));
+  
+      // Add the 'clicked' class to the clicked link
+      this.classList.add('clicked');
     });
-});
-
-// Add a 'clicked' class to social icons on click
-document.querySelectorAll('.social-icon').forEach((icon) => {
+  });
+  
+  // Add a 'clicked' class to social icons on click
+  document.querySelectorAll('.social-icon').forEach((icon) => {
     icon.addEventListener('click', () => {
-        icon.classList.add('clicked');
+      icon.classList.add('clicked');
     });
-});
-
-function toggleMenu() {
-    const menu = document.getElementById('nav-links');
-    menu.classList.toggle('hidden');
-}
+  });
